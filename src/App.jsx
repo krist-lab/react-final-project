eccc36ca82fc4c093af36d58315e3772ae079a27
krist-lab <qristina.beridze.15@gmail.com>
2025-09-03 09:43:34 +0400

    setup base React project with placeholder App

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white p-4">
+      Hello
+    </div>
+  );
+};
+
+export default App;
